7323ab85433fe669b41e999ea7741a73302e7c1b
cppxaxa <cppxaxa@gmail.com>
2018-09-25 03:00:07 +0530

    Automatic push

---
+++ DrawNoDependencies.py
@@ -0,0 +1,53 @@
+import cv2
+import json
+
+import time
+
+result = None
+output = None
+
+# Helper functions
+
+def ResultToJson(result):
+    result = str(result).replace("'", '"')
+    result = json.loads(result)
+    return result
+
+class DrawProcess:
+    Payload = ""
+    AlgorithmInSyntax = ""
+    Stopper = True
+    
+    def SetAlgorithmInSyntax(self, algorithmInSyntax: str):
+        self.AlgorithmInSyntax = algorithmInSyntax
+    
+    def SetPayload(self, payloadString : str):
+        self.Payload = payloadString
+        
+    def Draw(self, runOnce:bool = True, infinite:bool = False, limit: int = 1):
+        self.Stopper = False
+        c = 0
+        
+        if runOnce == True:
+            limit = 1
+            infinite = False
+        
+        while (c < limit):
+            globals()["output"] = None
+            globals()["result"] = None
+
+            exec(self.AlgorithmInSyntax)
+            exec(self.Payload, globals())
+
+            if infinite == False:
+                c += 1
+            
+            if self.Stopper == True:
+                break
+            
+            time.sleep(0.5)
+
+        return output
+    
+    def Stop(self):
+        self.Stopper = True
--- MockServer.py
+++ MockServer.py
@@ -1,6 +1,13 @@
 from flask import Flask
 from flask_restful import Resource, Api, reqparse
-import json
+import threading
+from DrawNoDependencies import DrawProcess
+
+
+process = DrawProcess()
+process.SetPayload("print(c)")
+
+task = None
 
 app = Flask(__name__)
 api = Api(app)
@@ -17,43 +24,41 @@ class TaskServer(Resource):
         parser.add_argument('ReturnResult', type=bool)
         args = parser.parse_args()
         
-        # RunOnce = args["RunOnce"]
-        # InfiniteLoop = args["InfiniteLoop"]
-        # LoopLimit = args["LoopLimit"]
+        RunOnce = args["RunOnce"]
+        InfiniteLoop = args["InfiniteLoop"]
+        LoopLimit = args["LoopLimit"]
         ReturnResult = args["ReturnResult"]
-        
-        if ReturnResult == False:
-            a = 10
-        else:
-            result = '[{"label":"cow","bottomright":{"y":354,"x":186},"topleft":{"y":267,"x":67},"confidence":0.5488251},{"label":"dog","bottomright":{"y":343,"x":209},"topleft":{"y":281,"x":122},"confidence":0.13515499},{"label":"horse","bottomright":{"y":365,"x":239},"topleft":{"y":260,"x":97},"confidence":0.18837829},{"label":"person","bottomright":{"y":374,"x":280},"topleft":{"y":104,"x":179},"confidence":0.6718636},{"label":"sheep","bottomright":{"y":336,"x":592},"topleft":{"y":145,"x":423},"confidence":0.83751935}]'
-            result = str(result).replace("'", '"')
-            result = json.loads(result)
-            return result, 200
-        
-        return "AckP", 200
-    
-    def delete(self):
-        return "AckD", 200
-
-class EchoServer(Resource):
-    def post(self):
-        parser = reqparse.RequestParser()
-        parser.add_argument('Fbp', type=list, action='append')
-        parser.add_argument('RunOnce', type=bool)
-        parser.add_argument('InfiniteLoop', type=bool)
-        parser.add_argument('LoopLimit', type=int)
-        parser.add_argument('ReturnResult', type=bool)
-        args = parser.parse_args()
         
         Fbp = []
         separator = ''
         for i in range(len(args["Fbp"])):
-            Fbp.append(separator.join(args["Fbp"][i]))        
+            Fbp.append(separator.join(args["Fbp"][i]))
         
-        result = { 'Fbp': Fbp, 'RunOnce': args['RunOnce'], 'InfiniteLoop': args['InfiniteLoop'], 'LoopLimit': args['LoopLimit'], 'ReturnResult': args['ReturnResult']}
-        return result, 200
+        for val in Fbp:
+            print(val)
+        
+        if len(Fbp) < 3:
+            return "Fbp should contain 3 lines", 200
+        
+        process.SetPayload(Fbp[2])
+        process.SetAlgorithmInSyntax(Fbp[1])
+        
+        global task
+        if task != None:
+            process.Stop()
+        
+        if ReturnResult == False:
+            task = threading.Thread(target=process.Draw, args=(RunOnce, InfiniteLoop, LoopLimit))
+            task.start()
+        else:
+            return (process.Draw(RunOnce, InfiniteLoop, LoopLimit)), 200
+        
+        return "AckP", 200
+    
+    def delete(self):
+        process.Stopper = True
+        return "AckD", 200
 
 api.add_resource(TaskServer, '/task')
-api.add_resource(EchoServer, '/echo')
 
 app.run(host = "0.0.0.0")
