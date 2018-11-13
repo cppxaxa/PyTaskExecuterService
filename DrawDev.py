a5b5e47973a473e600f64e876a2b9b5dc46b7e22
Christ Prateek Prasanna Xaxa <cppxaxa@live.com>
2018-11-13 22:27:12 +0530

    Automatic push

--- DrawDev.py
+++ DrawDev.py
@@ -6,12 +6,45 @@ import urllib.request as urllib
 import matplotlib.pyplot as plt
 from minimal_object_detection_lib import *
 
+import io
+import random
+
 import time
 
 result = None
 output = None
 
 # Helper functions
+def PostFile(filename, fileObj, Host, Port = 20000):
+    site = 'http://' + Host + ':' + str(Port) + '/MachineImageMessageApi'
+    file = {'image': (filename, fileObj)}
+    data = {}
+    request = requests.post(site, files=file, data=data)
+    
+def PostImage(filename, image, Host, Port = 20000):
+    retval, content = cv2.imencode('.jpg', image)
+    fileObj = io.BytesIO(content)
+    PostFile(filename, fileObj, 'localhost', 20000)
+    
+def GenerateDummyImage():
+    img = np.zeros((400,550,3), np.uint8)
+    x = random.randint(0,200)
+    y = random.randint(0,200)
+    s = random.randint(0,100)
+    cv2.rectangle(img,(250 + x,100 + y),(300 + x + s,150 + y + s),(255,255,255),3)
+    x = random.randint(0,200)
+    y = random.randint(0,200)
+    s = random.randint(0,100)
+    cv2.rectangle(img,(250 + x,100 + y),(300 + x + s,150 + y + s),(0,255,0),3)
+    return img
+
+def PostDummyImage(Host, Port = 20000):
+    filename = 'shot.jpg'
+    img = GenerateDummyImage()
+    PostImage(filename, img, 'localhost', 20000)
+
+
+
 
 def PostResult(Host, Payload, port = 20000, Uri = None):
     if Uri == None:
--- Workarounds/post multipart.py
+++ Workarounds/post multipart.py
@@ -1,21 +1,42 @@
 import requests
 import cv2
-from io import BytesIO
+import io
+import numpy as np
+import random
 
-def PostFile(site, filename, fileObj):    
+def PostFile(filename, fileObj, Host, Port = 20000):
+    site = 'http://' + Host + ':' + str(Port) + '/MachineImageMessageApi'
     file = {'image': (filename, fileObj)}
     data = {}
     request = requests.post(site, files=file, data=data)
+    
+def PostImage(filename, image, Host, Port = 20000):
+    retval, content = cv2.imencode('.jpg', image)
+    fileObj = io.BytesIO(content)
+    PostFile(filename, fileObj, 'localhost', 20000)
+    
+def GenerateDummyImage():
+    img = np.zeros((400,550,3), np.uint8)
+    x = random.randint(0,200)
+    y = random.randint(0,200)
+    s = random.randint(0,100)
+    cv2.rectangle(img,(250 + x,100 + y),(300 + x + s,150 + y + s),(255,255,255),3)
+    x = random.randint(0,200)
+    y = random.randint(0,200)
+    s = random.randint(0,100)
+    cv2.rectangle(img,(250 + x,100 + y),(300 + x + s,150 + y + s),(0,255,0),3)
+    return img
 
-site = 'http://localhost:20000/MachineImageMessageApi'
-filename = 'Banana.jpg'
-
-img = cv2.imread(filename)
-cv2.imshow('', img)
-cv2.waitKey()
+def PostDummyImage(Host, Port = 20000):
+    filename = 'shot.jpg'
+    img = GenerateDummyImage()
+    PostImage(filename, img, 'localhost', 20000)
 
 
+PostDummyImage('localhost', 20000)
 
-fileObj = open(filename, 'rb')
-print(fileObj)
-# PostFile(site, filename, fileObj)
+
+
+
+
+
